f8772e830879ae9305b57f5dcd770604cc281529
zloy <48566560+zloy4@users.noreply.github.com>
2025-06-16 01:49:48 +0300

    Update calc.sh

--- calc.sh
+++ calc.sh
@@ -1,56 +1,31 @@
 #!/bin/bash
+
 log_error() {
-    echo "$1" >> "$log_file"
+    echo "$(date) - $1" >> "$log_file"
     echo "$1" >&2
 }
 calculate() {
-    local operation=$1
-    shift
-    local numbers=("$@")
-    local result
-    case $operation in
-        sum)
-            result=0
-            for num in "${numbers[@]}"; do
-                result=$((result + num))
-            done
-            ;;
-        sub)
-            result=${numbers[0]}
-            for ((i=1; i<${#numbers[@]}; i++)); do
-                result=$((result - numbers[i]))
-            done
-            ;;
-        mul)
-            result=1
-            for num in "${numbers[@]}"; do
-                result=$((result * num))
-            done
-            ;;
-        div)
-            result=${numbers[0]}
-            for ((i=1; i<${#numbers[@]}; i++)); do
-                if [ "${numbers[i]}" -eq 0 ]; then
-                    log_error "Ошибка: деление на ноль"
-                    exit 1
-                fi
-                result=$((result / numbers[i]))
-            done
-            ;;
-        pow)
-            result=$((numbers[0] ** numbers[1]))
-            ;;
+    case $1 in
+        sum)   result=0; for num in "${@:2}"; do ((result += num)); done ;;
+        sub)   result=$2; for num in "${@:3}"; do ((result -= num)); done ;;
+        mul)   result=1; for num in "${@:2}"; do ((result *= num)); done ;;
+        div)   result=$2
+               for num in "${@:3}"; do
+                   ((num == 0)) && { log_error "Ошибка: деление на ноль"; exit 1; }
+                   ((result /= num))
+               done ;;
+        pow)   ((result = $2 ** $3)) ;;
+        *)     log_error "Неверная операция: $1"; exit 1 ;;
     esac
     echo "Результат: $result"
 }
 while getopts ":o:n:l:" opt; do
     case $opt in
         o) operation=$OPTARG ;;
-        n) numbers_str=$OPTARG ;;
+        n) IFS=' ' read -ra numbers <<< "$OPTARG" ;;
         l) log_file=$OPTARG ;;
-        \?) echo "Неверный ключ: -$OPTARG" >&2; exit 1 ;;
-        :) echo "Ключ -$OPTARG требует аргумента" >&2; exit 1 ;;
+        \?) log_error "Неверный ключ: -$OPTARG"; exit 1 ;;
+        :) log_error "Ключ -$OPTARG требует аргумента"; exit 1 ;;
     esac
 done
-IFS=' ' read -ra numbers <<< "$numbers_str"
 calculate "$operation" "${numbers[@]}"
